224433f33c02f89fe64b896a3a198e01612e42fe
shreedhar-ww <shreedhar.deshwal@gmail.com>
2022-02-11 12:21:12 +0530

    Code clean up

---
+++ test/ApplicationGateway.API.IntegrationTests/Helper/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationGateway.API.IntegrationTests.Helper
+{
+   
+
+    public static class ApplicationConstants
+    {
+        public const string TYK_BASE_URL = "http://localhost:8080/";
+        public const string BASE_PATH = "../../../JsonData/AccessRestrictionTest";
+    }
+}
