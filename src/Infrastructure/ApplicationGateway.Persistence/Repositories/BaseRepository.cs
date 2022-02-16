fd9c360b55293dc6386da7f63633c4f8c4141eb8
Himani-ja <himani.jain2309@gmail.com>
2022-02-16 18:55:11 +0530

    TransformerController

---
+++ src/Infrastructure/ApplicationGateway.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,58 @@
+﻿using ApplicationGateway.Application.Contracts.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationGateway.Persistence.Repositories
+{
+    public class BaseRepository<T> : IAsyncRepository<T> where T : class
+
+    {
+        protected readonly ApplicationDbContext _dbContext;
+        private readonly ILogger _logger;
+        public BaseRepository(ApplicationDbContext dbContext, ILogger<T> logger)
+        {
+            _dbContext = dbContext; _logger = logger;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public Task<IReadOnlyList<T>> GetPagedReponseAsync(int page, int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            _logger.LogInformation("ListAllAsync Initiated");
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
