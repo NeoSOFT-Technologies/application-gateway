863906fe9242e4aecca9ef065f896c89b331e5c9
avivekm <vivek.maurya@neosoftmail.com>
2022-02-16 15:41:04 +0530

    undo test comment

--- src/API/ApplicationGateway.Api/Controllers/v1/AccountController.cs
+++ src/API/ApplicationGateway.Api/Controllers/v1/AccountController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 
 namespace ApplicationGateway.Api.Controllers
 {
-//test comment
     [ApiVersion("1")]
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
