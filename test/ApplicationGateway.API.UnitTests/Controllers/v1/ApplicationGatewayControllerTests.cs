4a7166933cc20f5e24115da35a9dc66cc804c40e
avivekm <vivek.maurya@neosoftmail.com>
2022-03-08 10:03:37 +0530

    added unit test cases and less code smells

---
+++ test/ApplicationGateway.API.UnitTests/Controllers/v1/ApplicationGatewayControllerTests.cs
@@ -0,0 +1,116 @@
+﻿using ApplicationGateway.Api.Controllers;
+using ApplicationGateway.API.UnitTests.Mocks;
+using ApplicationGateway.Application.Features.Api.Commands.CreateApiCommand;
+using ApplicationGateway.Application.Features.Api.Commands.CreateMultipleApisCommand;
+using ApplicationGateway.Application.Features.Api.Commands.UpdateApiCommand;
+using ApplicationGateway.Application.Features.Api.Queries.GetAllApisQuery;
+using ApplicationGateway.Application.Features.Api.Queries.GetApiByIdQuery;
+using ApplicationGateway.Application.Responses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ApplicationGateway.API.UnitTests.Controllers.v1
+{
+    public class ApplicationGatewayControllerTests
+    {
+        private readonly Mock<IMediator> _mockMediator;
+        private readonly Mock<ILogger<ApplicationGatewayController>> _mockLogger;
+        public ApplicationGatewayControllerTests()
+        {
+            _mockLogger = new Mock<ILogger<ApplicationGatewayController>>();
+            _mockMediator = MediatorMocks.GetMediator();
+
+        }
+        [Fact]
+        public async Task Get_ApiList()
+        {
+            var controller = new ApplicationGatewayController(_mockMediator.Object, _mockLogger.Object);
+
+            var result = await controller.GetAllApis();
+
+            result.ShouldBeOfType<OkObjectResult>();
+            var okObjectResult = result as OkObjectResult;
+            okObjectResult.StatusCode.ShouldBe(200);
+            okObjectResult.Value.ShouldNotBeNull();
+            okObjectResult.Value.ShouldBeOfType<Response<GetAllApisDto>>();
+        }
+
+        [Fact]
+        public async Task Get_ApiById()
+        {
+            var controller = new ApplicationGatewayController(_mockMediator.Object,_mockLogger.Object);
+
+            var result = await controller.GetApiById(Guid.NewGuid());
+
+            result.ShouldBeOfType<OkObjectResult>();
+            var okObjectResult = result as OkObjectResult;
+            okObjectResult.StatusCode.ShouldBe(200);
+            okObjectResult.Value.ShouldNotBeNull();
+            okObjectResult.Value.ShouldBeOfType<Response<GetApiByIdDto>>();
+        }
+
+        [Fact]
+        public async Task Create_Api()
+        {
+            var controller = new ApplicationGatewayController(_mockMediator.Object,_mockLogger.Object);
+
+            var result = await controller.CreateApi(new CreateApiCommand());
+
+            result.ShouldBeOfType<OkObjectResult>();
+            var okObjectResult = result as OkObjectResult;
+            okObjectResult.StatusCode.ShouldBe(200);
+            okObjectResult.Value.ShouldNotBeNull();
+            okObjectResult.Value.ShouldBeOfType<Response<CreateApiDto>>();
+        }
+
+        [Fact]
+        public async Task Create_MultipleApi()
+        {
+            var controller = new ApplicationGatewayController(_mockMediator.Object, _mockLogger.Object);
+
+            var result = await controller.CreateMultipleApis(new CreateMultipleApisCommand());
+
+            result.ShouldBeOfType<OkObjectResult>();
+            var okObjectResult = result as OkObjectResult;
+            okObjectResult.StatusCode.ShouldBe(200);
+            okObjectResult.Value.ShouldNotBeNull();
+            okObjectResult.Value.ShouldBeOfType<Response<CreateMultipleApisDto>>();
+
+        }
+
+        [Fact]
+        public async Task Delete_Api()
+        {
+            var controller = new ApplicationGatewayController(_mockMediator.Object, _mockLogger.Object);
+
+            var result = await controller.DeleteApi(Guid.NewGuid());
+
+            result.ShouldBeOfType<NoContentResult>();
+            var noContentResult = result as NoContentResult;
+            noContentResult.StatusCode.ShouldBe(204);
+        }
+
+        [Fact]
+        public async Task Update_Api()
+        {
+            var controller = new ApplicationGatewayController(_mockMediator.Object, _mockLogger.Object);
+
+            var result = await controller.UpdateApi(new UpdateApiCommand());
+
+            result.ShouldBeOfType<OkObjectResult>();
+            var okObjectResult = result as OkObjectResult;
+            okObjectResult.StatusCode.ShouldBe(200);
+            okObjectResult.Value.ShouldNotBeNull();
+            okObjectResult.Value.ShouldBeOfType<Response<UpdateApiDto>>();
+        }
+    }
+}
---
+++ test/ApplicationGateway.API.UnitTests/Mocks/MediatorMocks.cs
@@ -0,0 +1,60 @@
+﻿using ApplicationGateway.Application.Features.Api.Commands.CreateApiCommand;
+using ApplicationGateway.Application.Features.Api.Commands.CreateMultipleApisCommand;
+using ApplicationGateway.Application.Features.Api.Commands.DeleteApiCommand;
+using ApplicationGateway.Application.Features.Api.Commands.UpdateApiCommand;
+using ApplicationGateway.Application.Features.Api.Queries.GetAllApisQuery;
+using ApplicationGateway.Application.Features.Api.Queries.GetApiByIdQuery;
+using ApplicationGateway.Application.Features.Key.Commands.CreateKeyCommand;
+using ApplicationGateway.Application.Features.Key.Commands.DeleteKeyCommand;
+using ApplicationGateway.Application.Features.Key.Commands.UpdateKeyCommand;
+using ApplicationGateway.Application.Features.Key.Queries.GetAllKeys;
+using ApplicationGateway.Application.Features.Key.Queries.GetKey;
+using ApplicationGateway.Application.Responses;
+using ApplicationGateway.Domain.Entities;
+using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApplicationGateway.API.UnitTests.Mocks
+{
+    public class MediatorMocks
+    {
+        public static Mock<IMediator> GetMediator()
+        {
+            var mockMediator = new Mock<IMediator>();
+            //api
+            mockMediator.Setup(m => m.Send(It.IsAny<GetAllApisQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(new Response<GetAllApisDto>());
+            mockMediator.Setup(m => m.Send(It.IsAny<GetApiByIdQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(new Response<GetApiByIdDto>());
+
+
+            mockMediator.Setup(m => m.Send(It.IsAny<CreateApiCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync(new Response<CreateApiDto>());
+            mockMediator.Setup(m => m.Send(It.IsAny<CreateMultipleApisCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync(new Response<CreateMultipleApisDto>());
+           
+            mockMediator.Setup(m => m.Send(It.IsAny<DeleteApiCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync(new Unit());
+
+            mockMediator.Setup(m => m.Send(It.IsAny<UpdateApiCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync(new Response<UpdateApiDto>());
+
+            //key
+            mockMediator.Setup(m => m.Send(It.IsAny<GetAllKeysQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(new Response<GetAllKeysDto>());
+            mockMediator.Setup(m => m.Send(It.IsAny<GetKeyQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(new Response<Key>());
+
+            mockMediator.Setup(m => m.Send(It.IsAny<CreateKeyCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync(new Response<Key>());
+            mockMediator.Setup(m => m.Send(It.IsAny<DeleteKeyCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync(new Unit());
+            mockMediator.Setup(m => m.Send(It.IsAny<UpdateKeyCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync(new Response<UpdateKeyCommandDto>());
+
+
+
+
+
+            return mockMediator;
+
+
+
+        }
+    }
+}
