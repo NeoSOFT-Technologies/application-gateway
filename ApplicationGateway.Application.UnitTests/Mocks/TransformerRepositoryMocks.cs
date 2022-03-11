8e2c6cfea969af8271c4fe7cb83b318d705b634c
avivekm <vivek.maurya@neosoftmail.com>
2022-03-11 19:27:49 +0530

    transformer query handler tests

---
+++ ApplicationGateway.Application.UnitTests/Mocks/TransformerRepositoryMocks.cs
@@ -0,0 +1,43 @@
+﻿using ApplicationGateway.Application.Contracts.Persistence;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationGateway.Application.UnitTests.Mocks
+{
+    public class TransformerRepositoryMocks
+    {
+        public static Mock<ITransformerRepository> GetTransformerRepository()
+        {
+            var transformers = new List<Domain.Entities.Transformer>()
+            {
+                new Domain.Entities.Transformer()
+                {
+                    TransformerId = Guid.NewGuid(),
+                    TemplateName =  "TemplateName1",
+                    TransformerTemplate = "TransformerTemplate2",
+                    Gateway = new Domain.Entities.Gateway(){},
+                },
+                new Domain.Entities.Transformer()
+                {
+                    TransformerId = Guid.NewGuid(),
+                    TemplateName =  "TemplateName2",
+                    TransformerTemplate = "TransformerTemplate2",
+                    Gateway = new Domain.Entities.Gateway(){},
+                }
+            };
+            var mockTransformerRepository = new Mock<ITransformerRepository>();
+            mockTransformerRepository.Setup(repo => repo.ListAllAsync()).ReturnsAsync(transformers);
+            mockTransformerRepository.Setup(repo => repo.GetByIdAsync(It.IsAny<Guid>())).ReturnsAsync(
+                (Guid id) =>
+                {
+                    return transformers.SingleOrDefault(x => x.TransformerId == id);
+                }
+                );
+            return mockTransformerRepository;
+        }
+    }
+}
