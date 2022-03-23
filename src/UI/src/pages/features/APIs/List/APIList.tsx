8e038b2ac96427bab0e0d0288bbf7181cc9a85f5
thakwanigovind <govind.thakwani@datatusk.com>
2022-03-23 18:38:07 +0530

    Fixed Date Format & Status

--- src/UI/src/pages/features/APIs/List/APIList.tsx
+++ src/UI/src/pages/features/APIs/List/APIList.tsx
@@ -5,10 +5,14 @@ import { useNavigate } from "react-router-dom";
 import { RootState } from "../../../../store";
 import { useAppDispatch, useAppSelector } from "../../../../store/hooks";
 import { getApiList } from "../../../../store/features/api/list/slice";
-import { IApiDataList, IApiListState } from "../../../../types/api/index";
+import {
+  IApiData,
+  IApiDataList,
+  IApiListState,
+} from "../../../../types/api/index";
 import Spinner from "../../../../components/loader/Loader";
 import { useErrorHandler } from "react-error-boundary";
-// import moment from "moment";
+import moment from "moment";
 
 function Bomb() {
   console.log("");
@@ -46,20 +50,20 @@ export default function APIList() {
   };
   useEffect(() => {
     // console.log("UseEffect", apiList.data);
-    // apiList.data?.Apis.forEach((item) => {
-    //   // if (item.IsActive === true) {
-    //   //   item.Status = "Active";
-    //   // } else {
-    //   //   item.Status = "In-Active";
-    //   // }
-    //   if (item.CreatedDate !== "") {
-    //     item.CreatedDate = moment(item.CreatedDate).format("DD/MM/YYYY");
-    //   }
-    // });
-    if (apiList.data) {
+    if (apiList.data && apiList.data?.Apis?.length > 0) {
+      const listAPI: IApiData[] = [];
+      apiList.data?.Apis.forEach((item) => {
+        const listObj = Object.create(item);
+        listObj.Status = listObj.IsActive === true ? "Active" : "In-Active";
+        listObj.CreatedDateTxt =
+          listObj.CreatedDate !== ""
+            ? moment(listObj.CreatedDate).format("DD/MM/YYYY")
+            : "";
+        listAPI.push(listObj);
+      });
       setDataList({
-        list: [...apiList.data.Apis],
-        fields: ["Name", "TargetUrl", "IsActive", "CreatedDate"],
+        list: [...listAPI],
+        fields: ["Name", "TargetUrl", "Status", "CreatedDateTxt"],
       });
     }
   }, [apiList.data]);
