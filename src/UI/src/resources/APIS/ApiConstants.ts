1c40400b6064a69737df4c8f2e138649f81642fc
Himani-ja <himani.jain2309@gmail.com>
2022-03-29 17:37:46 +0530

    Fixed issues in ApiConstant

--- src/UI/src/resources/APIS/ApiConstants.ts
+++ src/UI/src/resources/APIS/ApiConstants.ts
@@ -1,7 +1,4 @@
-import {
-  setFormError,
-  setForm,
-} from "../../store/features/api/update-state/slice";
+import { setFormError, setForm } from "../../store/features/api/getById/slice";
 export const regexForName = /^[A-Z a-z]{1,29}$/;
 export const regexForListenPath = /^[/][a-zA-Z0-9]*[/]$/; // "//" + "[a-z]*" + "//"; // /^[a-z]*ing$/i; (\/)(A-Z a-z)?(\/)
 export const regexForTagetUrl =
