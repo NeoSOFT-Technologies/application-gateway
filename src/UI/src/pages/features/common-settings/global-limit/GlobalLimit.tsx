85a8cd07b437f472a5db4e80b39a3bd999df0fb6
avivekm <vivek.maurya@neosoftmail.com>
2022-04-08 16:00:11 +0530

    create key UI minor changes

--- src/UI/src/pages/features/common-settings/global-limit/GlobalLimit.tsx
+++ src/UI/src/pages/features/common-settings/global-limit/GlobalLimit.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Form, Row } from "react-bootstrap";
 export default function GlobalLimit() {
+  const [rate, setRate] = useState(false);
+  const [throttle, setThrottle] = useState(true);
+  const [quota, setQuota] = useState(true);
+
   return (
     <>
       <div className="card">
@@ -27,101 +31,133 @@ export default function GlobalLimit() {
               >
                 <div className="accordion-body ">
                   <Row>
-                    <Col md="4">
-                      <Form.Group className="mb-3">
-                        <Form.Label>
-                          <b>Rate Limiting</b>
-                        </Form.Label>
-                        <Form.Check
-                          type="switch"
-                          id="disableGlobalRate"
-                          name="GlobalLimit.IsDisabled"
-                          label="Disable rate limiting"
-                        />
-                        <Form.Label>Rate</Form.Label>
-                        <br />
-                        <Form.Control
-                          className="mt-2"
-                          type="text"
-                          id="rate"
-                          placeholder="Enter Rate"
-                          name="RateLimit.Rate"
-                        />
-                        <Form.Label>Per (Seconds)</Form.Label>
-                        <br />
-                        <Form.Control
-                          className="mt-2"
-                          type="text"
-                          id="rate"
-                          placeholder="Enter time"
-                          name="RateLimit.Per"
-                        />
-                      </Form.Group>
-                    </Col>
-                    <Col md="4">
-                      <Form.Group className="mb-3">
-                        <Form.Label>
-                          <b>Throttling</b>
-                        </Form.Label>
-                        <Form.Check
-                          type="switch"
-                          id="disableThrottling"
-                          name="Throttling.IsDisabled"
-                          label="Disable Throttling"
-                        />
-                        <Form.Label>Throttle retry limit</Form.Label>
-                        <br />
-                        <Form.Control
-                          className="mt-2"
-                          type="text"
-                          id="rate"
-                          placeholder="Enter retry limit"
-                          name="Throttling.Retry"
-                        />
-                        <Form.Label>Throttle interval</Form.Label>
-                        <br />
-                        <Form.Control
-                          className="mt-2"
-                          type="text"
-                          id="rate"
-                          placeholder="Enter interval"
-                          name="Throttling.Interval"
-                        />
-                      </Form.Group>
-                    </Col>
-                    <Col md="4">
-                      <Form.Group className="mb-3">
-                        <Form.Label>
-                          <b>Usage Quota</b>
-                        </Form.Label>
-                        <Form.Check
-                          type="switch"
-                          id="unlimitedRequests"
-                          name="unlimitedRequests.IsDisabled"
-                          label="Unlimited requests"
-                        />
-                        <Form.Label>Max requests per period</Form.Label>
-                        <br />
-                        <Form.Control
-                          className="mt-2"
-                          type="text"
-                          id="rate"
-                          placeholder="Enter request per period"
-                          name="Quota.Per"
-                        />
-                        <Form.Label> Quota resets every</Form.Label>
-                        <Form.Select className="mt-2" style={{ height: 45 }}>
-                          <option>never</option>
-                          <option>1 hour</option>
-                          <option>6 hour</option>
-                          <option>12 hour</option>
-                          <option>1 week</option>
-                          <option>1 month</option>
-                          <option>6 months</option>
-                          <option>12 months</option>
-                        </Form.Select>
-                      </Form.Group>
-                    </Col>
+                    <Row>
+                      <Col md="4">
+                        <Form.Group className="mb-3">
+                          <Form.Label className="mt-2">
+                            <b>Rate Limiting</b>
+                          </Form.Label>
+                          <Form.Check
+                            type="switch"
+                            id="disableGlobalRate"
+                            name="GlobalLimit.IsDisabled"
+                            label="Disable rate limiting"
+                            checked={rate}
+                            onChange={(e: any) => setRate(e.target.checked)}
+                          />
+                          <Form.Label className="mt-3">Rate</Form.Label>
+                          <br />
+                          <Form.Control
+                            className="mt-2"
+                            type="text"
+                            id="rate"
+                            placeholder="Enter Rate"
+                            name="RateLimit.Rate"
+                            disabled={rate}
+                          />
+                          <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+                          <Form.Label className="mt-3">
+                            Per (Seconds)
+                          </Form.Label>
+                          <br />
+                          <Form.Control
+                            className="mt-2"
+                            type="text"
+                            id="rate"
+                            placeholder="Enter time"
+                            name="RateLimit.Per"
+                            disabled={rate}
+                          />
+                          <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+                        </Form.Group>
+                      </Col>
+                      <Col md="4">
+                        <Form.Group className="mb-3">
+                          <Form.Label className="mt-2">
+                            <b>Throttling</b>
+                          </Form.Label>
+                          <Form.Check
+                            type="switch"
+                            id="disableThrottling"
+                            name="Throttling.IsDisabled"
+                            label="Disable Throttling"
+                            checked={throttle}
+                            onChange={(e: any) => setThrottle(e.target.checked)}
+                          />
+                          <Form.Label className="mt-3">
+                            Throttle retry limit
+                          </Form.Label>
+                          <br />
+                          <Form.Control
+                            className="mt-2"
+                            type="text"
+                            id="rate"
+                            placeholder="Enter retry limit"
+                            name="Throttling.Retry"
+                            disabled={throttle}
+                          />
+
+                          <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+                          <Form.Label className="mt-3">
+                            Throttle interval
+                          </Form.Label>
+                          <br />
+                          <Form.Control
+                            className="mt-2"
+                            type="text"
+                            id="rate"
+                            placeholder="Enter interval"
+                            name="Throttling.Interval"
+                            disabled={throttle}
+                          />
+                        </Form.Group>
+                      </Col>
+                      <Col md="4">
+                        <Form.Group className="mb-3">
+                          <Form.Label className="mt-2">
+                            <b>Usage Quota</b>
+                          </Form.Label>
+                          <Form.Check
+                            type="switch"
+                            id="unlimitedRequests"
+                            name="unlimitedRequests.IsDisabled"
+                            label="Unlimited requests"
+                            checked={quota}
+                            onChange={(e: any) => setQuota(e.target.checked)}
+                          />
+                          <Form.Label className="mt-3">
+                            Max requests per period
+                          </Form.Label>
+                          <br />
+                          <Form.Control
+                            className="mt-2"
+                            type="text"
+                            id="rate"
+                            placeholder="Enter request per period"
+                            name="Quota.Per"
+                            disabled={quota}
+                          />
+                          <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
+                          <Form.Label className="mt-3">
+                            Quota resets every
+                          </Form.Label>
+                          <Form.Select
+                            className="mt-2"
+                            style={{ height: 46 }}
+                            disabled={quota}
+                          >
+                            <option>never</option>
+                            <option>1 hour</option>
+                            <option>6 hour</option>
+                            <option>12 hour</option>
+                            <option>1 week</option>
+                            <option>1 month</option>
+                            <option>6 months</option>
+                            <option>12 months</option>
+                          </Form.Select>
+                        </Form.Group>
+                      </Col>
+                    </Row>
                   </Row>
                 </div>
               </div>
