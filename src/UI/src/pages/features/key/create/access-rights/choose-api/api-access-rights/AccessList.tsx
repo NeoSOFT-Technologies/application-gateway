df9770abf8b6fcbf743553b8df2fd62dceb15ee0
NikitaBhoir <bhoir.nikita.18ce5504@gmail.com>
2022-04-08 17:09:04 +0530

    small update

--- src/UI/src/pages/features/key/create/access-rights/choose-api/api-access-rights/AccessList.tsx
+++ src/UI/src/pages/features/key/create/access-rights/choose-api/api-access-rights/AccessList.tsx
@@ -1,5 +1,100 @@
-import React from "react";
+import { Grid } from "gridjs-react";
+import React, { useEffect } from "react";
+import { RootState } from "../../../../../../../store";
+import { IApiListState } from "../../../../../../../store/features/api/list";
+import { getApiList } from "../../../../../../../store/features/api/list/slice";
+// import { IKeyCreateState } from "../../../../../../../store/features/key/create";
+import {
+  useAppDispatch,
+  useAppSelector,
+} from "../../../../../../../store/hooks";
 
 export default function AccessList() {
-  return <div> api list</div>;
+  // const createState: IKeyCreateState = useAppSelector(
+  //   (rootState: RootState) => rootState.addKeyState
+  // );
+
+  const apiList: IApiListState = useAppSelector(
+    (state: RootState) => state.apiListState
+  );
+  const dispatch = useAppDispatch();
+  const mainCall = async (currentPage: number) => {
+    dispatch(getApiList({ currentPage }));
+  };
+  useEffect(() => {
+    mainCall(1);
+  }, []);
+
+  // const es: IApiData[] = apiList.data?.Apis!;
+
+  console.log(apiList);
+  const grid = new Grid({
+    columns: [
+      {
+        name: "Name",
+        attributes: (cell: any) => {
+          if (cell) {
+            return {
+              "data-cell-content": cell,
+              onclick: () => alert(cell),
+              style: "cursor: pointer",
+            };
+          }
+        },
+      },
+      "Target Url",
+      "CreatedDate",
+    ],
+    data: {
+      ...apiList.data,
+      fields: ["Name", "TargetUrl", "Status", "CreatedDateTxt"],
+    },
+    // apiList.data?.Apis.map((data) => [
+    // data.Name,
+    // data.TargetUrl,
+    // data.CreatedDate,
+    // ])
+    className: {
+      container: "table-wrapper",
+    },
+  });
+
+  return (
+    <div>
+      <div className="card mb-3">
+        <div>
+          <div className="align-items-center justify-content-around">
+            <div className="accordion" id="accordionSetting">
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="headingOne">
+                  <button
+                    className="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    API List
+                  </button>
+                </h2>
+                <div
+                  id="collapseOne"
+                  className="accordion-collapse collapse show"
+                  aria-labelledby="headingOne"
+                  data-bs-parent="#accordionSetting"
+                >
+                  <div className="accordion-body">
+                    <div>
+                      <Grid {...grid.props} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/UI/src/pages/features/key/create/access-rights/choose-api/api-access/ApiAccess.tsx
+++ src/UI/src/pages/features/key/create/access-rights/choose-api/api-access/ApiAccess.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import GlobalLimit from "../../../../../common-settings/global-limit/GlobalLimit";
 import PathBased from "../../../../../common-settings/path-based-permission/PathBased";
 
 export default function ApiAccess() {
   return (
     <div>
-      <h5>api access Setting</h5>
-      <GlobalLimit />
       <PathBased />
     </div>
   );
