baae583f9db8aed7c3da0d1228bb480ef83a8e4f
alfaizkhan <42858836+alfaiz12@users.noreply.github.com>
2022-04-11 11:35:26 +0530

    Delete src/UI/src/services/Policy directory

--- src/UI/src/services/Policy/policy.ts
+++
@@ -1,6 +0,0 @@
-// import { IApiData } from "../../types/api/index";
-import apiFactory from "../../utils/api";
-
-export function policyListService(currentPage: number) {
-  return apiFactory().get(`Policy?pageNum=${currentPage}&pageSize=3`);
-}
