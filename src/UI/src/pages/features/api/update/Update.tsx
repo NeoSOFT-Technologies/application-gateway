d70d448bec782ab836dd1e516bd1bcf0bba70a76
Hemanth6569 <hemanthmedala6569>
2022-04-13 11:15:30 +0530

    Styling changes

--- src/UI/src/pages/features/api/update/Update.tsx
+++ src/UI/src/pages/features/api/update/Update.tsx
@@ -55,7 +55,7 @@ export default function Update() {
             >
               <div className="align-items-center">
                 <div
-                  className="card-header bg-white mt-3 pt-2 pb-2"
+                  className="card-header bg-white mt-3 pt-1 pb-4"
                   style={{ padding: "0.5rem 1.5rem" }}
                 >
                   <button className=" btn btn-sm btn-success btn-md d-flex float-right mb-3">
