f65aabbc7b564d27a63fbddc11a8b5010aeef3c5
thakwanigovind <govind.thakwani@datatusk.com>
2022-04-21 11:22:54 +0530

    Global Limit - Apply Policy

--- src/UI/src/pages/features/key/create/access-rights/apply-policy/policies/Policies.tsx
+++ src/UI/src/pages/features/key/create/access-rights/apply-policy/policies/Policies.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Accordion } from "react-bootstrap";
 import { useAppSelector } from "../../../../../../../store/hooks";
 import GlobalLimit from "../../../../../common-settings/global-limit/GlobalLimit";
 
@@ -8,30 +9,33 @@ export default function Policies() {
     <>
       <br />
       <br /> <br /> <h5>Policies</h5> <br />
-      <div className="card col-lg-12 grid-margin stretch-card">
-        {state.data.form.Policies !== null &&
-        state.data.form.Policies.length > 0 &&
-        Array.isArray(state.data.form.Policies) ? (
-          (state.data.form.Policies as any[]).map(
-            (data: any, index: number) => {
-              // const { policies } = data;
-              return (
-                <div key={index}>
-                  <GlobalLimit
-                    isDisabled={true}
-                    msg={""}
-                    policyId={data}
-                    index={index}
-                  />
-                </div>
-              );
-            }
-          )
-        ) : (
-          <></>
-        )}{" "}
-        <br /> <br />
-      </div>{" "}
+      {state.data.form.Policies !== null &&
+      state.data.form.Policies.length > 0 &&
+      Array.isArray(state.data.form.Policies) ? (
+        (state.data.form.Policies as any[]).map((data: any, index: number) => {
+          // const { policies } = data;
+          return (
+            <div key={index}>
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Policy Name</Accordion.Header>
+                  <Accordion.Body>
+                    <GlobalLimit
+                      isDisabled={true}
+                      msg={""}
+                      policyId={data}
+                      index={index}
+                    />
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </div>
+          );
+        })
+      ) : (
+        <></>
+      )}{" "}
+      <br /> <br />{" "}
     </>
   );
 }
