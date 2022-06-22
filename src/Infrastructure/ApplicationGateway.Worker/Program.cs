babe2ca15695912259e740273ed9b2420a37bc4a
Apoorv Rane <apoorv.rane@neosoftmail.com>
2022-06-22 11:21:20 +0530

    Added logs to worker service

--- src/Infrastructure/ApplicationGateway.Worker/Program.cs
+++ src/Infrastructure/ApplicationGateway.Worker/Program.cs
@@ -1,9 +1,25 @@
 using ApplicationGateway.Application.Models.Tyk;
 using ApplicationGateway.Worker;
+using Serilog;
 using StackExchange.Redis;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((hostContext, services) =>
+var builder = Host.CreateDefaultBuilder(args);
+IConfiguration configurationBuilder = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile(
+        $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+        optional: true)
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configurationBuilder)
+    .CreateBootstrapLogger().Freeze();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configurationBuilder)
+    .CreateLogger();
+
+IHost host = builder.ConfigureServices((hostContext, services) =>
     {
         var configuration = hostContext.Configuration;
         services.AddHostedService<Worker>();
@@ -11,8 +27,21 @@ IHost host = Host.CreateDefaultBuilder(args)
         services.AddSingleton<IConnectionMultiplexer>(multiplexer);
         services.Configure<TykConfiguration>(configuration.GetSection("TykConfiguration"));
     })
+    .UseSerilog((ctx, lc) => lc
+        .WriteTo.Console()
+        .ReadFrom.Configuration(ctx.Configuration))
     .Build();
 
-
+using (var scope = host.Services.CreateScope())
+{
+    try
+    {
+        Log.Information("Application Starting");
+    }
+    catch (Exception ex)
+    {
+        Log.Warning(ex, "An error occured while starting the application");
+    }
+}
 
 await host.RunAsync();
